93ee04670f44e4665595be13cceaf2f0ff530a90
Allan Burns <allanlburns.gmail.com>
2024-11-07 13:18:46 -0500

    Project structure added from Spring Initializer

---
+++ src/main/java/allan/org/arkham_horror_app/ArkhamHorrorAppApplication.java
@@ -0,0 +1,13 @@
+package allan.org.arkham_horror_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkhamHorrorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkhamHorrorAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/allan/org/arkham_horror_app/ArkhamHorrorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package allan.org.arkham_horror_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkhamHorrorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
