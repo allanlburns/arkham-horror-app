502861a98d98d6271fa115651e9b37b9f20ba8b8
Allan Burns <allanlburns.gmail.com>
2025-01-06 12:11:41 -0500

    Create model for Encounter Card

--- src/main/java/allan/org/arkham_horror_app/model/EncounterCard.java
+++ src/main/java/allan/org/arkham_horror_app/model/EncounterCard.java
@@ -1,4 +1,23 @@
 package allan.org.arkham_horror_app.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class EncounterCard extends Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String neighborhood;
+    private String number;
+    private String location1;
+    private String location2;
+    private String location3;
+    @Column(columnDefinition = "TEXT")
+    private String location1Text;
+    @Column(columnDefinition = "TEXT")
+    private String location2Text;
+    @Column(columnDefinition = "TEXT")
+    private String location3Text;
+    private String set;
 }
