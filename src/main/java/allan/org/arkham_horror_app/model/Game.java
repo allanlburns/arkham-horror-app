9abbe13d4dca55bc473ccc0d30bf440264cde142
Allan Burns <allanlburns.gmail.com>
2025-02-27 11:33:08 -0500

    Define Game class

--- src/main/java/allan/org/arkham_horror_app/model/Game.java
+++ src/main/java/allan/org/arkham_horror_app/model/Game.java
@@ -1,4 +1,16 @@
 package allan.org.arkham_horror_app.model;
 
+import java.util.List;
+
 public class Game {
+    int id;
+//    String name;
+    String scenario;
+    String scenarioSheet;
+    List<EncounterCard> encounterDeck;
+    List<EventCard> eventDeck;
+    List<MonsterCard> monsterDeck;
+    List<HeadlineCard> headlineDeck;
+    List<MythosToken> mythosCup;
+
 }
